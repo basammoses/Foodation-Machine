da044b3227e9467c7e239fcf66eed2263b6bcccf
Basam Hassan <basammoses@gmail.com>
2023-02-02 12:03:01 -0500

    update 2

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # PROJECT1
 Nutrition Planner
 giving randomized recipes based on user needs and dietary restrictions. i would like to transition it to a more complex sorting function once the baseline is created for the website
+
+Wednsday - complete propsal and initialize javascript segment of project
+
+
+Thursday - finailize Javascript and reassure no errors or flaw in code, HTML FRAME OR ROUGH DRAFT COMPLETED
+
+
+Friday -  Clean up HTML and make it look asthetically pleasing and asssure functionality with JS.
+
+SAT - SUN/ work on small tweaks to improve on MVP
--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'c71af68df7mshd2357862033bf0ep127bf3jsn73c3e1bc8818',
-		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+		'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
 	}
 };
+const foodpics = document.getElementById('foodpic')
+const select = document.querySelector('select')
+//let userInput = ''
+const urlInput = 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr='
 
-fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian%2Cdessert&number=1', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+async function getFoodInfo() {
+  const res = await fetch(`${urlInput}{}`, options)
+  const json = await res.json()
+  console.log(json)
+
+}
+  
+
+async function FoodInfo(id) {
+  const res = fetch(`${urlInput, id}`, options)
+  const json = await res.json()
+  console.log(json)
+  return json
+  
+}
+
+select.addEventListener('change', async e => {
+  const food = await FoodInfo(e.target.value)
+  console.log(food)
+
+})
+  
+
+
+
