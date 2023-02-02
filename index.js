54cb2425500ccf67d9920780bf7ce1f9cbb9c2ae
Basam Hassan <basammoses@gmail.com>
2023-02-02 15:24:52 -0500

    update3

--- index.js
+++ index.js
@@ -5,33 +5,65 @@ const options = {
 		'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
 	}
 };
+const input = document.querySelector('input')
 const foodpics = document.getElementById('foodpic')
 const select = document.querySelector('select')
 //let userInput = ''
+const form = document.querySelector('form')
+let results = document.getElementsByClassName('results')
+
 const urlInput = 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr='
 
-async function getFoodInfo() {
-  const res = await fetch(`${urlInput}{}`, options)
+
+
+form.addEventListener('submit', async element => {
+  element.preventDefault()
+  let userInput = input.value
+  const res = await fetch(`${urlInput}${userInput}`, options)
+  console.log(res)
   const json = await res.json()
+  //console.log(input.value)
+  //console.log(userInput)
   console.log(json)
-
-}
   
-
-async function FoodInfo(id) {
-  const res = fetch(`${urlInput, id}`, options)
-  const json = await res.json()
-  console.log(json)
-  return json
   
-}
-
-select.addEventListener('change', async e => {
-  const food = await FoodInfo(e.target.value)
-  console.log(food)
-
+  // results.innerHTML = json
+  // let text = '';
+  // for (let parsed in json) {
+  //   text += json[parsed]
+  //   console.log(text)
+    
+  // }
+  
+    
 })
   
 
+  // async function FoodInfo(id) {
+  //   const res = fetch(`${urlInput, id}`, options)
+  //   const json = await res.json()
+  //   console.log(json)
+  //   return json
+  
+  // }
+
+  // form.addEventListener('submit', async e => {
+  
+  
+  //   const food = await FoodInfo()
+  //   console.log(food)
+
+  // })
+  
 
 
+  // const handleClick = async () => {
+  //   const info = await getMovieDetails(imbdID)
+  //   console.log(info)
+  // }
+
+  // async function getFoodDetails(input) {
+  //   const res = await fetch(`http://www.omdbapi.com/?s=${input}&apikey=62a37e61`)
+  //   const json = await res.json()
+  //   return json
+  //
